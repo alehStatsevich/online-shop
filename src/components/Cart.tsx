6443ec5bb01a050571572ecc2b2c3172491f8daf
olegStacevich <oleg295832611@gmail.com>
2021-09-14 17:18:41 +0200

    basket

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,20 +1,19 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "../state/store";
 import style from "./Cart.module.css";
 import {
     addProductAC,
-    addToCartAC,
     deleteAllProductAC,
     deleteProductAC,
-    initialStateType
 } from "../state/product-reducer";
-import {ProductType} from "./Product";
 import HeaderList from "../header/HeaderList";
+import styleContainer from "../common/Conteiner.module.css";
+
 type CartPropsType = {
     title: string
 }
-const Cart = (props:CartPropsType) => {
+const Cart = (props: CartPropsType) => {
     const elementsForBasket = useSelector((state: AppRootStateType) => state.products.elementsForBasket)
 
     const [total, setTotal] = useState(0)
@@ -22,7 +21,7 @@ const Cart = (props:CartPropsType) => {
     const dispatch = useDispatch()
 
     useEffect(() => {
-        const totalPrice = elementsForBasket.reduce((acc,curr) => {
+        const totalPrice = elementsForBasket.reduce((acc, curr) => {
             return acc + +curr.product.price.priceFormatted.slice(1) * curr.count
         }, 0);
         setTotal(totalPrice)
@@ -35,34 +34,37 @@ const Cart = (props:CartPropsType) => {
 
     return (<>
         <HeaderList title={props.title}/>
-        <div>
-           ${total}
-        </div>
         {elementsForBasket.length ?
-            <div style={{width: 500, height: 1000}} className="product">
+            <div className={`${styleContainer.container} ${style.cart}`}>
                 {elementsForBasket.map((el) => {
                     return (
-                        <tr key={el.product.id}>
-                            <td>{el.product.image}</td>
-                            <td>{el.product.name}</td>
-                            <td>
-                                <button className={style.dec}
-                                        onClick={() => dispatch(deleteProductAC(el.product.id))}>-
-                                </button>
-                                // {el.count} //
-
-                                <button className={style.inc}
-                                        onClick={() => dispatch(addProductAC(el.product.id))}>+</button>
-                            </td>
-                            <td>{`$ ${+el.product.price.priceFormatted.slice(1) * el.count}`}</td>
-                            <td>
-                                <button className={style.del} onClick={() => dispatch(deleteAllProductAC(el.product.id))}>x</button>
-                            </td>
-                        </tr>
+                            <tr key={el.product.id} className={style.cartBlock}>
+                                <td>{el.product.image}</td>
+                                <td>{el.product.name}</td>
+                                <td className={style.block}>
+                                    <div className={style.dec}
+                                            onClick={() => dispatch(deleteProductAC(el.product.id))}>-
+                                    </div>
+                                    <div className={style.num}>
+                                        {el.count}
+                                    </div>
+                                    <div className={style.inc}
+                                            onClick={() => dispatch(addProductAC(el.product.id))}>+
+                                    </div>
+                                </td>
+                                <td>{`$ ${+el.product.price.priceFormatted.slice(1) * el.count}`}</td>
+                                <td>
+                                    <div className={style.del}
+                                         onClick={() => dispatch(deleteAllProductAC(el.product.id))}>x
+                                    </div>
+                                </td>
+                            </tr>
                     )
                 })}
-
-            </div> : <div>j</div>
+                <div>
+                    Total ${total}
+                </div>
+            </div> : <div>your basket empty</div>
         }
 
     </>);
